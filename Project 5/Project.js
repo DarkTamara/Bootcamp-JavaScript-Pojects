8e674fdc482ecb20e15a2d1a9fb031f459fa113a
AndreeaM <miron.andreea12345@gmail.com>
2021-08-10 15:05:36 +0300

    Project 5

---
+++ Project 5/Project.js
@@ -0,0 +1,19 @@
+var neoBackgroundt = document.getElementById("background");
+var neotextColor = document.getElementById("textColor");
+var neofont = document.getElementById("font");
+var body = document.querySelector("body");
+
+neoBackgroundt.addEventListener('keyup', function(event){
+    body.style.backgroundColor = event.target.value;
+});
+
+neotextColor.addEventListener('keyup', function(e){
+    body.style.color = e.target.value;
+});
+
+neofont.addEventListener('keyup',function(e){
+    console.log(e. target.value);
+    body.style.fontFamily = e.target.value;
+    console.log("font is: ", body.style.fontFamily);
+
+})
